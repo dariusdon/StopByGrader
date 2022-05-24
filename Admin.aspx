e4f583aa07a206c2dac9a58f912b5d9eb5463ec9
Darius-Florin Don <uie38743@contiwan.com>
2022-05-24 22:08:38 +0300

    Raport Admin

--- Admin.aspx
+++ Admin.aspx
@@ -12,6 +12,21 @@
              <p style="margin-left:48%">
                 <asp:Label ID="Label1" Font-Size="38px" Font-Bold="true" ForeColor="Orange" runat="server" Text="Admin"></asp:Label>
             </p>
+            <asp:Label style="margin-left:100px" ID="Label2" Font-Size="25px" Font-Bold="true" ForeColor="Orange" runat="server" Text="Raport Operatii:"></asp:Label>
+            <br />
+             <asp:GridView style="margin-left:100px" ID="GridView1" Width="1750px" AllowPaging="true" Font-size="22px"  runat="server" HeaderStyle-BackColor="#ffa500" HeaderStyle-ForeColor="Black" PageSize="20"  onpageindexchanging="GrdEmp_PageIndexChanging">
+         
+         <PagerSettings mode="Numeric"
+          position="Bottom"           
+          pagebuttoncount="10" />
+         <PagerStyle backcolor="#ffa500"
+          height="30px"
+          verticalalign="Bottom"
+          horizontalalign="Right"/>
+          </asp:GridView>
+            <br />
+            <br />
+            <asp:Button style="margin-left:47%" ID="Button1" Width="150px" Font-Size="23px" ForeColor="Red" runat="server" Text="Delogare" OnClick="Logout" />
         </div>
     </form>
 </body>
--- Admin.aspx.cs
+++ Admin.aspx.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +13,77 @@ public partial class Admin : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        Details();
+    }
+    public void Details()
+    {
+        string stop = ConfigurationManager.ConnectionStrings["stop"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(stop))
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                string sql = @"Select *from dbo.StopByGrader order by DataOperatie desc";
 
+                cmd.CommandText = sql;
+                cmd.Connection = con;
+                using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                {
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    GridView1.AllowPaging = true;
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+            }
+        }
+    }
+    protected void GrdEmp_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        GridView1.PageIndex = e.NewPageIndex;
+        GridView1.DataBind();
+    }
+    public static void ToCSV(DataTable dtDataTable, string strFilePath)
+    {
+        StreamWriter sw = new StreamWriter(strFilePath, false);
+        //headers    
+        for (int i = 0; i < dtDataTable.Columns.Count; i++)
+        {
+            sw.Write(dtDataTable.Columns[i]);
+            if (i < dtDataTable.Columns.Count - 1)
+            {
+                sw.Write(",");
+            }
+        }
+        sw.Write(sw.NewLine);
+        foreach (DataRow dr in dtDataTable.Rows)
+        {
+            for (int i = 0; i < dtDataTable.Columns.Count; i++)
+            {
+                if (!Convert.IsDBNull(dr[i]))
+                {
+                    string value = dr[i].ToString();
+                    if (value.Contains(','))
+                    {
+                        value = String.Format("\"{0}\"", value);
+                        sw.Write(value);
+                    }
+                    else
+                    {
+                        sw.Write(dr[i].ToString());
+                    }
+                }
+                if (i < dtDataTable.Columns.Count - 1)
+                {
+                    sw.Write(",");
+                }
+            }
+            sw.Write(sw.NewLine);
+        }
+        sw.Close();
+
+    }
+    protected void Logout(object sender,EventArgs e)
+    {
+        Response.Redirect("Default.aspx");
     }
 }
